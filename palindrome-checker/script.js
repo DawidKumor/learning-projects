d295e5f0fe1d84b3cb1425b0a939292ecb1a72f2
DawidKumor <dawidkumor@gmail.com>
2026-04-14 00:11:08 +0200

    palindrome-checker regex added
    ignoring non-alphanumeric characters in regex

--- palindrome-checker/script.js
+++ palindrome-checker/script.js
@@ -6,9 +6,9 @@ button.addEventListener("click", () => {
     return alert("Please input a value");
     
   }
-  let reverse = textInput.value.split("").reverse().join("")
-  if (textInput.value === reverse) {
+  const text = textInput.value.replace(/[^a-z0-9]/gi,"").toLowerCase()
+  const reverse = text.split("").reverse().join("");
+  if (text === reverse) {
 result.innerText = `${textInput.value} is a palindrome`
-  } else if(textInput.value !== reverse) 
-  {result.innerText = `${textInput.value} is not a palindrome`}
+  } else {result.innerText = `${textInput.value} is not a palindrome`}
 })
