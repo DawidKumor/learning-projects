31cdaf36237991b0a97eab3da50dd98eab05fedd
DawidKumor <dawidkumor@gmail.com>
2026-04-15 01:20:52 +0200

    palindrome-checker css styles added

--- palindrome-checker/script.js
+++ palindrome-checker/script.js
@@ -1,6 +1,7 @@
 const button = document.getElementById("check-btn");
 const textInput = document.getElementById("text-input");
-const result = document.getElementById("result")
+const result = document.getElementById("result");
+const palindromeDef = document.querySelector(".palindrome-definition");
 button.addEventListener("click", () => {
   if (textInput.value === "") {
     return alert("Please input a value");
@@ -9,6 +10,15 @@ button.addEventListener("click", () => {
   const text = textInput.value.replace(/[^a-z0-9]/gi,"").toLowerCase()
   const reverse = text.split("").reverse().join("");
   if (text === reverse) {
+    result.removeAttribute("hidden")
 result.innerText = `${textInput.value} is a palindrome`
-  } else {result.innerText = `${textInput.value} is not a palindrome`}
+  } else {
+    result.removeAttribute("hidden")
+    result.innerText = `${textInput.value} is not a palindrome`}
+})
+button.addEventListener("mouseenter", () => {
+    palindromeDef.classList.add("palindrome-colored");
+})
+button.addEventListener("mouseleave", () => {
+    palindromeDef.classList.remove("palindrome-colored");
 })
