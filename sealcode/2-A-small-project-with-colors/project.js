165ad37cdfd43aa1b9f254844cabfabb571ed799
DawidKumor <dawidkumor@gmail.com>
2026-06-03 01:50:50 +0200

    project 2 update

--- sealcode/2-A-small-project-with-colors/project.js
+++ sealcode/2-A-small-project-with-colors/project.js
@@ -1,6 +1,15 @@
+import chalk from 'chalk';
 function colorCount(k) {
     for (let i = 1; i <= k; i++) {
-       console.log(i) ;
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log(chalk.hex('#7f3098')(i))
+        } else if (i % 3 === 0) {
+            console.log(chalk.red(i));
+        } else if (i % 5 === 0) {
+            console.log(chalk.blue(i));
+        } else {
+            console.log(chalk.white(i));
+        }
     }
 }
 colorCount(100);
