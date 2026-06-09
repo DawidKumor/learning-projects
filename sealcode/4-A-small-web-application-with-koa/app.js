ecae7c38a15867ce83fca1dee5c8a4d50592311a
DawidKumor <dawidkumor@gmail.com>
2026-06-10 01:56:37 +0200

    4-A-small-web-application-with-express

---
+++ sealcode/4-A-small-web-application-with-koa/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
+
+app.get("/hello", (req, res) => {
+    res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>HELLO, world</h1>
+    
+</body>
+</html>`)
+});
+
+app.get("/goodbye", (req, res) => {
+    res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Goodbye, world!</h1>
+    
+</body>
+</html>`)
+})
