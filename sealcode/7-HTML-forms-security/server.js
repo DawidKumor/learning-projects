b6d9371aac073ea509338593042a67798a5e7eb8
DawidKumor <dawidkumor@gmail.com>
2026-06-12 01:25:58 +0200

    7-HTML update

--- sealcode/7-HTML-forms-security/server.js
+++ sealcode/7-HTML-forms-security/server.js
@@ -1,10 +1,46 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+//const bodyParser = require("body-parser");
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app
+  .route("/login")
+  .get((req, res) => {
+    res.sendFile(__dirname + "/views/login.html");
+  })
+  .post((req, res) => {
+    if (req.body.username === "admin" && req.body.password === "adminadmin") {
+      res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>logged in!</h1>
+    
+</body>
+</html>`);
+    } else {
+      res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Wrong username or password</h1>
+    
+</body>
+</html>`);
+    }
+  });
+
+app.use(express.static("views"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
